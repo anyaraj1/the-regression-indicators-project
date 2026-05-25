84735c3919ca96be029aad1af32a75a09138766d
anyaraj1 <anyar2@uci.edu>
2026-05-25 14:33:37 -0700

    added class visualization plots

--- knn_classifier/kNN.py
+++ knn_classifier/kNN.py
@@ -12,3 +12,13 @@ X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
 
 X_train_flat = X_train / 255.0
 X_test_flat = X_test / 255.0
+
+fig, axes = plt.subplots(2, 5, figsize = (10,4))
+for i, ax in enumerate(axes.flat):
+    idx = np.where(y_train == i)[0][0]
+    ax.imshow(X_train[idx].reshape(28, 28), cmap = 'gray')
+    ax.set_title(labels[i])
+    ax.axis('off')
+plt.tight_layout()
+plt.savefig("01_data_preview_knn.png")
+print("Saved the data preview to '01_data_preview_knn.png")
